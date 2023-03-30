e96af6725b98225197c91bb965395d18a9a0c16e
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2023-03-30 19:19:00 +0500

    changed theme input type to text

--- src/pages/main/components/messageform/index.tsx
+++ src/pages/main/components/messageform/index.tsx
@@ -97,7 +97,7 @@ const MESSAGEFORM: FC = () => {
                     }
                     <div className="relative mb-6" data-te-input-wrapper-init="">
                         <input
-                            type="email"
+                            type="text"
                             className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                             id="exampleInput8"
                             placeholder="Email address"
